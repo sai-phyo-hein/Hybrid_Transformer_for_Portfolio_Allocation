584612dbb635f825f02b6fd73fa223400b096fba
sai_phyo_hein <63901802+sai-phyo-hein@users.noreply.github.com>
2024-05-19 21:08:46 +0630

    Update eq_data_loader.py
    adding docstring and comments

--- data/eq_data_loader.py
+++ data/eq_data_loader.py
@@ -8,25 +8,34 @@ def get_eq_data(data_path, start, end, corr_thresh, market_cap_filter):
     A function that select the equities out of nifty50 based on these criteria: 
         - equity is traded during selected date range and no blank
         - all selected equities have mutual correlation less than a threshold
-
-    return: DataFrame 
+        
+    @param data_path: path to a csv file listing all available stocks and related information
+    @param start: start date of data to retreive
+    @param end: end date of data to retreive
+    @param corr_thresh: correlation threshold for stock selection
+    @param market_cap_filter: number of stocks to select from top market caps
+    @return: dataframe: a dataframe of price data of selected stocks
+    
     """
     nf_50_tickers_df = pd.read_csv(data_path)[['Industry', 'Symbol']]
     nf_50_tickers_df.Symbol = nf_50_tickers_df.Symbol + '.NS'
     nf_50_tickers_df['market_cap'] = [yf.Ticker(tick).get_info()['marketCap'] for tick in nf_50_tickers_df.Symbol]
 
+    # get historical data of the selected stocks
     data = yf.download(
         nf_50_tickers_df.Symbol.tolist(), 
         start = start, 
         end = end, 
     )['Adj Close'].dropna(axis = 1)
 
+    # filtering the stocks by the correlation threshold
     corr = data.pct_change().dropna(axis = 0).corr() 
     for i in range(corr.shape[0]): 
         corr.iloc[i, i] = 0.0
     corr_lt_thresh = corr[corr < corr_thresh]
     corr_lt_thresh.dropna(thresh = corr.shape[1], axis = 1, inplace = True)
 
+    # getting top n of stockers from each industry
     selected_tickers = nf_50_tickers_df[
         nf_50_tickers_df.Symbol.isin(corr_lt_thresh.columns)
     ].sort_values(
